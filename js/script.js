01f2e3501fcd7490f6e26e572fa12177b1d7bfea
gothamlies <shaneez.osu@gmail.com>
2021-09-03 22:55:20 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ document.querySelectorAll('.tabs-triggers__item').forEach((item) =>
         document.querySelectorAll('.tabs-content__item').forEach(
             (child) => child.classList.remove('tabs-content__item--active')
         );
-
         item.classList.add('tabs-triggers__item--active');
         document.getElementById(id).classList.add('tabs-content__item--active');
 
